ecb9c279a1c13885906b94c15cbb8279d9d79513
EC2 Default User <ec2-user@ip-172-31-46-25.us-east-2.compute.internal>
2018-05-22 22:52:16 +0000

    sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'https://rubytest-.herokuapp.com/', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://rubytest-.herokuapp.com/' }
 
 end
