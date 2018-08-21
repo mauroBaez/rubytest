ab767602652e52155e37c1131b78ccfb2bd98787
EC2 Default User <ec2-user@ip-172-31-7-208.us-east-2.compute.internal>
2018-08-21 07:30:26 +0000

    cloudinary

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,5 +1,6 @@
 require "shrine"
-
+require "cloudinary"
+require "shrine/storage/cloudinary"
 
 Shrine.plugin :activerecord
 Shrine.plugin :backgrounding
@@ -12,47 +13,29 @@ Shrine.plugin :upload_endpoint
 
 
 if Rails.env.production?
-  require "./config/cloudinarycredentials"
+  
 
-  require "shrine/storage/cloudinary"
-  
-  #require "./jobs/promote_job"
-  #require "./jobs/delete_job"
-  
-  Cloudinary.config(
-    cloud_name: ENV.fetch("CLOUDINARY_CLOUD_NAME"),
-    api_key:    ENV.fetch("CLOUDINARY_API_KEY"),
-    api_secret: ENV.fetch("CLOUDINARY_API_SECRET"),
-  )
-  
-  Shrine.storages = {
-    cache: Shrine::Storage::Cloudinary.new(prefix: "cache"),
-    store: Shrine::Storage::Cloudinary.new(prefix: "store"),
-  }
-  
-  Shrine.plugin :backgrounding
-  Shrine.plugin :logging
-  
-  
-#  Shrine.plugin :presign_endpoint, presign_options: { method: :put }
-#  s3_options = {
-#    access_key_id:     "uuuu",
-#    secret_access_key: "uuuuu",
-#    region:            "us-east-2",
-#    bucket:            "papita",
-#  }
-#  Shrine.storages = {
-#    cache: Shrine::Storage::S3.new(prefix: "cache", **s3_options),
-#    store: Shrine::Storage::S3.new(**s3_options),
-#  }
+Cloudinary.config(
+  cloud_name: "aurelio",
+  api_key:    "367271932682191",
+  api_secret: "MjVw1Rnj9wINNAk8bOo6X5uGt_E",
+)
 
+Shrine.storages = {
+  cache: Shrine::Storage::Cloudinary.new(prefix: "cache"), # for direct uploads
+  store: Shrine::Storage::Cloudinary.new(prefix: "store"),
+}
 else
-  require "shrine/storage/file_system"
+  Cloudinary.config(
+  cloud_name: "aurelio",
+  api_key:    "367271932682191",
+  api_secret: "MjVw1Rnj9wINNAk8bOo6X5uGt_E",
+)
 
-  Shrine.storages = {
-    cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary
-    store: Shrine::Storage::FileSystem.new("public", prefix: "uploads"),       # permanent
-  }
+Shrine.storages = {
+  cache: Shrine::Storage::Cloudinary.new(prefix: "cache"), # for direct uploads
+  store: Shrine::Storage::Cloudinary.new(prefix: "store"),
+}
 end
 
 Shrine::Attacher.promote { |data| PromoteJob.perform_async(data) }
