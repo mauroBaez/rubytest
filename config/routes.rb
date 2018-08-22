8c881edc468d063d6e2c75841e49d2af397b14c9
EC2 Default User <ec2-user@ip-172-31-7-208.us-east-2.compute.internal>
2018-08-22 21:54:09 +0000

    fotos

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
   get 'pages/about'
   
   mount Shrine.upload_endpoint(:cache) => "/upload"
-  
+  #mount ImageUploader::UploadEndpoint => "/upload"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
