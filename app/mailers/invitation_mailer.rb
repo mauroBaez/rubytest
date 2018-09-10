2e269c6f920e714066d8b4096daa193f0889f096
EC2 Default User <ec2-user@ip-172-31-7-208.us-east-2.compute.internal>
2018-09-10 01:23:50 +0000

    dd

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,7 +1,7 @@
 class InvitationMailer < ActionMailer::Base
     default :from => 'giypablo@mailgun.giypablo.com'
 
-    def send_invitation_email(guest)
+    def send_invitation_email(guest,invitados)
         mail(:to => guest.email, :subject => 'All the Monies!')
     end
 end
