ea713a737088276e7fc2e4a034842ac5c47a0c34
EC2 Default User <ec2-user@ip-172-31-7-208.us-east-2.compute.internal>
2018-10-01 23:44:05 +0000

    lklklk

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -2,6 +2,6 @@ class Invitation < ApplicationRecord
     
     has_many :guests, -> { order(position: :asc) }, dependent: :delete_all
     accepts_nested_attributes_for :guests, allow_destroy: true
-    has_many :sent_emails, -> { order(position: :asc) }, dependent: :delete_all
+    has_many :sent_emails, -> { order(id: :asc) }, dependent: :delete_all
     accepts_nested_attributes_for :sent_emails, allow_destroy: true
 end
